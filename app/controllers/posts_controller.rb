7b40ef85d4177feb64b10e24b65866bc96488520
Adam Morgan <aymorgan@hotmail.co.uk>
2015-12-31 13:25:02 +0000

    Fixed image issue within post controller and added linked images to show and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,12 +28,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.update(post_params)
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
   def destroy
     @post.destroy
